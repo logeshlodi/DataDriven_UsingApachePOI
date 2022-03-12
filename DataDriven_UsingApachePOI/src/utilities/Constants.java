634ace7f8d81074ffc1dddfa9c207124995b774e
logeshlodi <logesh0793@gmail.com>
2022-03-12 14:28:49 +0530

    Executed Properly

--- DataDriven_UsingApachePOI/src/utilities/Constants.java
+++ DataDriven_UsingApachePOI/src/utilities/Constants.java
@@ -1,7 +1,7 @@
 package utilities;
- 
+
 public class Constants {
-    public static final String URL = "https://demoqa.com/automation-practice-form";
-    public static final String Path_TestData = "E:\\Java Workplace\\DataDriven_UsingApachePOI\\src\\testData\\";	
-    public static final String File_TestData = "TestData.xlsx";
+	public static final String URL = "https://demoqa.com/automation-practice-form";
+	public static final String Path_TestData = "src\\testData\\";	
+	public static final String File_TestData = "TestData.xlsx";
 }
